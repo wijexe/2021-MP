e032128d0451367d727403ed85bd210ba36c7165
MasterChulepa <83087560+MasterChulepa@users.noreply.github.com>
2021-05-01 10:55:05 +0300

    Update Server.py

--- 427/xXx_Парные_копыта_xXx/Server.py
+++ 427/xXx_Парные_копыта_xXx/Server.py
@@ -1,15 +1,12 @@
 import socket
-import queue
 import Filter
 import File_Executor
-
-
+import Moderations
 
 class Server():
-    def __init__(self, host, port,  recvPackets = queue.Queue()):
+    def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.recvPackets = recvPackets
         open('logs.txt', 'w').close()
 
     def getHost(self):
@@ -18,15 +15,21 @@ class Server():
     def getPort(self):
         return self.port
 
-    def getQueue(self):
-        return self.recvPackets
-
-    def receive(self, sock, recvPackets):
+    def receive(self, sock):
         fileExecutor = File_Executor.FileExecutor()
         filter_class = Filter.Filter()
+        moderator = Moderations.Moderator()
         while True:
             data,addr = sock.recvfrom(1024)
-            recvPackets.put((data, addr))
+            name = filter_class.censor(data.decode('utf-8')).split(' ')[0][1:-1]
+            clients = moderator.create_data(name)
+            if filter_class.censor(data.decode('utf-8')).endswith("/admin"):
+                clients = moderator.become_an_admin(name)
+                fileExecutor.writeDown_log(name + ' ' + "become an administrator")
+                continue
+            moderator.admin(name, filter_class.censor(data.decode('utf-8')))
+            if clients[name][0][0]:
+                continue
             fileExecutor.writeDown_log(filter_class.censor(data.decode('utf-8')))
 
     def runServer(self):
@@ -35,18 +38,12 @@ class Server():
         server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         server.bind((self.getHost(),self.getPort()))
         print('Server Running...')
-        self.receive(server, self.getQueue())
-
-    def greetings(self,i):
-        f = open('name.txt', 'r')
-        lines = f.read().splitlines()
-        name = lines[i]
-        message = name + " joined the chat!"
-        return message
+        self.receive(server)
 
 
 host = socket.gethostbyname(socket.gethostname())
 port = 5000
 server = Server(host, port,)
 server.runServer()
+
 
